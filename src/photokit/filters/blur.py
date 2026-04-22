47cb943a32459b6694770aea8406691360ea87b3
Trex <amiramiritabat01@gmail.com>
2026-04-22 19:12:08 +0300

    duzeltdim

--- src/photokit/filters/blur.py
+++ src/photokit/filters/blur.py
@@ -5,6 +5,6 @@ from .base import Filter
 class BlurFilter(Filter):
     name = "blur"
 
-    def apply(self, img: Image.Image, **kwargs) -> Image.Image:
+    async def apply(self, img: Image.Image, **kwargs) -> Image.Image:
         radius = kwargs.get("radius", 2)
         return img.filter(ImageFilter.GaussianBlur(radius=radius))
